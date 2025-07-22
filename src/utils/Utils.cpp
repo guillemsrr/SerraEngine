7c91879b6ae5ce0b3a81c5a0ca0531cdcf64c294
Guillem Serra <guillemserralorenz@gmail.com>
2025-07-22 16:06:13 +0200

    Add Utils class with LengthSquared method for vector calculations

---
+++ src/utils/Utils.cpp
@@ -0,0 +1,8 @@
+﻿#include "Utils.h"
+
+#include <glm/detail/func_geometric.inl>
+
+float Utils::LengthSquared(const glm::vec3& v)
+{
+    return glm::dot(v, v);
+}
---
+++ src/utils/Utils.h
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Guillem Serra. All Rights Reserved.
+
+#pragma once
+#include <glm/vec3.hpp>
+
+class Utils
+{
+public:
+    static float LengthSquared(const glm::vec3& v);
+};
