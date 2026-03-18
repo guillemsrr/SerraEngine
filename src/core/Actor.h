55dbf5369ecb11edb479e4328a09ba87a97bd40c
Guillem Serra <guillemserralorenz@gmail.com>
2026-03-18 09:45:58 +0100

    using quaternions for rotations and adding scale property

--- src/core/Actor.h
+++ src/core/Actor.h
@@ -1,7 +1,10 @@
 // Copyright (c) Guillem Serra. All Rights Reserved.
 
 #pragma once
-#include <glm/vec3.hpp>
+#include <glm/fwd.hpp>
+#include <glm/detail/type_quat.hpp>
+
+#include <glm/gtc/quaternion.hpp>
 
 class Actor
 {
@@ -10,5 +13,9 @@ public:
     virtual ~Actor() = default;
 
     glm::vec3 Position = glm::vec3(0.0f);
-    glm::vec3 Rotation = glm::vec3(0.0f);
+    glm::quat Rotation = glm::quat();
+    glm::vec3 Scale = glm::vec3(1.0f);
+
+    void SetRotation(const glm::quat& rotation) { Rotation = rotation; }
+    void SetRotation(const glm::mat3& rotation) { Rotation = glm::quat_cast(rotation); }
 };
