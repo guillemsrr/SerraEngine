0b2b5c902f38b8c541556b7cda06fcf8155e46e1
Guillem Serra <guillemserralorenz@gmail.com>
2026-09-12 07:47:08 +0200

    Add `AudioEmitter` class and `AudioSettings` struct to support audio configuration and data handling

---
+++ src/audio/AudioEmitter.h
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Guillem Serra. All Rights Reserved.
+
+#pragma once
+
+#include "core/Actor.h"
+#include "AudioData.h"
+#include "AudioSettings.h"
+
+class AudioEmitter : public Actor
+{
+public:
+    AudioEmitter() = default;
+
+    void SetAudioData(const AudioData& audioData) { _audioData = audioData; }
+    const AudioData& GetAudioData() const { return _audioData; }
+
+    void SetAudioSettings(const AudioSettings& audioSettings) { _audioSettings = audioSettings; }
+    const AudioSettings& GetAudioSettings() const { return _audioSettings; }
+    AudioSettings& GetAudioSettings() { return _audioSettings; }
+
+private:
+    AudioData _audioData;
+    AudioSettings _audioSettings;
+};
---
+++ src/audio/AudioSettings.h
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Guillem Serra. All Rights Reserved.
+
+#pragma once
+
+struct AudioSettings
+{
+    float Range = 16.0f;
+    int MaxBounces = 3;
+    int MaxReflectionCandidates = 2;
+    float AlignmentThreshold = 0.78f;
+    float Volume = 1.0f;
+};
